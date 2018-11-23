c03fd97dd81bf6e145b1a6ec5e37445f3e6fb76b
IZARBE LANGA GARDE <izlangar@macb0606.abbaa.upv.es>
2018-11-23 13:45:26 +0100

    Initial Commit

---
+++ tarea_izarbe_langa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tarea_izarbe_langa
+//
+//  Created by IZARBE LANGA GARDE on 23/11/2018.
+//  Copyright © 2018 IZARBE LANGA GARDE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
